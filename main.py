167c14490772ebd9c3131f16adfaa60d2b23f2f7
collins-ruto <collinsruto48@gmail.com>
2022-05-21 12:00:49 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-print("      Christmas comes early with python")
+print("Christmas comes early with python")
 
-h = 1
-i = 1
-k = 21
-s = 1
-r = 1
-while k > 10 and h < 2:
-     print(20 * " " + h * "⭐" )
-     h += 1
-     k -= 1
-h = 1
+h = 1    # Head
+i = 1    # Body
+k = 20   # Spacing
+s = 1    # Stem
+r = 1    # Root
+
+# Head
+print(20 * " " + h * "⭐" )
+
+# Body section     
 while i < 20 and k > 1:
     print(k * " " + i * "*" + i * "*" )
     i += 1
     k -= 1
+
+# Stem section
 if i == 20 and k == 1 :
     while s < 3:
         print(19 * " " + 3 * "*" + 2 * "*")
         s += 1
+# Root / Base section
     while r < 3:
         print(12 * " " + 9 * "*" + 10 * "*")
         r += 1
